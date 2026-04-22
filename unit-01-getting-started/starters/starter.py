9cd56240bee9245b110e524e61027a22794eaa2f
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-22 10:36:36 -0400

    Update starter.py

--- unit-01-getting-started/starters/starter.py
+++ unit-01-getting-started/starters/starter.py
@@ -1,1 +1,15 @@
-# Starter File
+# Insert first and last name
+# Insert current date
+# Variables & Print Function Practice
+
+# TODO 1: Define a Python variable named first_name and assign it your first name
+
+# TODO 2: Define another Python variable named school and assign it the name of your home school
+
+# TODO 3: Define two more Python variables and use them to describe yourself, e.g., hair color, age, nationality, etc.
+
+# TODO 4: Use the print( ) function to display your first name
+
+# TODO 5: On the next line, use the print( ) function to display the name of your school
+
+# TODO 6: Use the print ( ) function to display the other two pieces of information about yourself (see TODO 3)
