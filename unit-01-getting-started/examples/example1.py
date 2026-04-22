f9812307e1368f02351476105594d0e267ecc008
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-22 10:28:46 -0400

    Update example1.py

--- unit-01-getting-started/examples/example1.py
+++ unit-01-getting-started/examples/example1.py
@@ -2,7 +2,16 @@
 # Current date
 # Project Title
 
+# Example 1
+# Display a simple greeting using the built-in print ( ) function
+print('Hello, world!')
+
+
+# Example 2
+# Store user's first and last name in Python variables
 first_name = 'Abraham'
 last_name = 'Lincoln'
+# Build an f-string (like a fill-in-the-blank sentence)
 full_name = f'{first_name} {last_name}'
+# Display the user's full name
 print(full_name)
