fff76b27936755144e27a7b4c18122ebf5280514
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-05-06 10:52:12 -0400

    Add variable declarations and print full name

--- unit-02-variables/examples/example1.py
+++ unit-02-variables/examples/example1.py
@@ -2,7 +2,10 @@
 # Current date
 # Project Title
 
+# Declaring and assigning a value to two variables
 first_name = 'Abraham'
 last_name = 'Lincoln'
+# Assigning an f-string to the full_name variable
 full_name = f'{first_name} {last_name}'
+# Displaying the contents of full_name variable
 print(full_name)
