216b31e863e8b9b71a40d31a8133eaf7d4c6990d
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-05-06 11:23:19 -0400

    Create basic-math-starter.py

---
+++ unit-02-variables/starters/basic-math-starter.py
@@ -0,0 +1,16 @@
+# First and last name
+# Current date
+# Basic Math in Python
+
+# --- PART 1 --- #
+# Write addition, subtraction, multiplication and division operations
+# that each result in the number 8.  
+# Use short, descriptive variable names (such as num1, num2, etc.) in your equations
+# Use the print ( ) function and f-strings to display your answers
+
+
+
+# --- PART 2 --- #
+# Define a variable that represents your lucky number
+# Use this variable to create a short message that reveals your lucky number
+# Use the print ( ) function and an f-string to display your lucky number
