df851a48f00c5cce35faf0b575988129a04b82a4
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-05-06 10:56:36 -0400

    Update starter.py with comments and TODO tasks
    Added comments and TODOs for character creation.

--- unit-02-variables/starters/starter.py
+++ unit-02-variables/starters/starter.py
@@ -1,1 +1,13 @@
-# Starter File
+# First and last name
+# Current date
+# Hero Stats Card 
+
+# TODO 1: Start by updating your comment block at the top of this script
+
+# TODO 2: Declare and assign values to three variables for a game character
+# For example, name, health_points and power
+
+# TODO 3: Declare a forth variable named 'message' and assign an f-string containing
+# the three facts about your game character
+
+# TODO 4: Use print function to display the message/output string
