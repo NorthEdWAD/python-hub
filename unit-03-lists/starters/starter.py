62e3f34fe0327e938ff2be7af7605330c7e59817
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-06-05 07:52:37 -0400

    Edited Python starter file code

--- unit-03-lists/starters/starter.py
+++ unit-03-lists/starters/starter.py
@@ -1,1 +1,9 @@
-# Starter File
+# First and last name
+# Current date
+# Project name/title
+
+# This is a Python comment
+
+# The print statement below has been 'commented out' (turned into a comment)
+# To uncomment the line of code, remove the # from the beginning of the line of code
+# print("Good morning!")
