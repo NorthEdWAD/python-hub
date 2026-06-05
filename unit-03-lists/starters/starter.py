bb14cdf91bc6276faa90faced5223739f8739424
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-06-05 08:35:03 -0400

    Edited starter file for lists

--- unit-03-lists/starters/starter.py
+++ unit-03-lists/starters/starter.py
@@ -8,14 +8,14 @@
 # To uncomment the line of code, remove the # from the beginning of the line of code
 # print("Good morning!")
 
-# TODO: Write code for Try It Yourself 3-1 (Names) below this line
+# TODO 1: Write code for Try It Yourself 3-1 (Names) below this line
 
 
 
 
-# TODO: Write code for Try It Yourself 3-2 (Greetings) below this line
+# TODO 2: Write code for Try It Yourself 3-2 (Greetings) below this line
 
 
 
 
-# TODO: Write code for Try It Yourself 3-3 (Your Own List) below this line
+# TODO 3: Write code for Try It Yourself 3-3 (Your Own List) below this line
