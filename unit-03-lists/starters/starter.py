06894af61a141da9729cb819feac882a5bc48b0a
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-06-05 08:34:37 -0400

    Added TODOs to starter for lists

--- unit-03-lists/starters/starter.py
+++ unit-03-lists/starters/starter.py
@@ -7,3 +7,15 @@
 # The print statement below has been 'commented out' (turned into a comment)
 # To uncomment the line of code, remove the # from the beginning of the line of code
 # print("Good morning!")
+
+# TODO: Write code for Try It Yourself 3-1 (Names) below this line
+
+
+
+
+# TODO: Write code for Try It Yourself 3-2 (Greetings) below this line
+
+
+
+
+# TODO: Write code for Try It Yourself 3-3 (Your Own List) below this line
