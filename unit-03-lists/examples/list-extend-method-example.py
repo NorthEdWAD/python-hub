b5c4d1b16a60d8f12c1bfc4a580cd6eba590e976
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-06-09 08:03:34 -0400

    Create list-extend-method-example.py

---
+++ unit-03-lists/examples/list-extend-method-example.py
@@ -0,0 +1,11 @@
+# Abraham Lincoln
+# 20 JUN 2026
+# Python Extend List Method Example
+
+
+fruits = ["apple", "banana"]
+more_fruits = ["cherry", "date"]
+
+fruits.extend(more_fruits)
+print(fruits)  
+# Output: ['apple', 'banana', 'cherry', 'date']
